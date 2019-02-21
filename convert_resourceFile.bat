c610f44b2e4ec921b2d1a2a0bc7fbeb5c99f3680
Kenneth C. Kleissl <kenneth_kleissl@hotmail.com>
2019-02-21 07:41:53 +0100

    added move up/down arrows for geometry table

--- convert_resourceFile.bat
+++ convert_resourceFile.bat
@@ -1,3 +1,4 @@
 "C:\Users\Kenneth\Anaconda3\Library\bin\pyside2-rcc.exe" -o resources_rc.py resources.qrc
 "C:\Users\kekl\Anaconda3\Library\bin\pyside2-rcc.exe" -o resources_rc.py resources.qrc
+"C:\Users\kekl\AppData\Local\Programs\Python\Python37-32\Lib\site-packages\PySide2\pyside2-rcc.exe" -o resources_rc.py resources.qrc
 pause

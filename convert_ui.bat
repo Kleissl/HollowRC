a44723c9a14622c8fbed4e2e10bf76b11ec0834f
Kenneth C. Kleissl <kenneth_kleissl@hotmail.com>
2019-09-05 22:20:29 +0200

    utilizing LOCALAPPDATA for cleaner path

--- convert_ui.bat
+++ convert_ui.bat
@@ -1,6 +1,4 @@
-"C:\Users\Kenneth\Anaconda3\Library\bin\pyside2-uic.bat" -x hollow_window.ui -o hollow_window.py
-"C:\Users\kekl\Anaconda3\Library\bin\pyside2-uic.bat" -x hollow_window.ui -o hollow_window.py
 "C:\Program Files (x86)\Python37-32\Scripts\pyside2-uic.exe" -x hollow_window.ui -o hollow_window.py
-"C:\Users\kekl\AppData\Local\Programs\Python\Python37-32\Scripts\pyside2-uic.exe" -x hollow_window.ui -o hollow_window.py
-"C:\Users\kekl\AppData\Local\Programs\Python\Python37\Scripts\pyside2-uic.exe" -x hollow_window.ui -o hollow_window.py
+"%LOCALAPPDATA%\Programs\Python\Python37-32\Scripts\pyside2-uic.exe" -x hollow_window.ui -o hollow_window.py
+"%LOCALAPPDATA%\Programs\Python\Python37\Scripts\pyside2-uic.exe" -x hollow_window.ui -o hollow_window.py
 pause

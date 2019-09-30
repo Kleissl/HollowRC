b1b2f3cb16d9924fc95b1e9eb7ea9eb5b088c8ea
Kenneth C. Kleissl <kenneth_kleissl@hotmail.com>
2019-10-01 01:09:09 +0200

    disabling stiffness when not applicable

--- Material.py
+++ Material.py
@@ -2,15 +2,13 @@
 """
 Class definition of a container for material properties
 
-History log:
-Version 0.1 - first working build
-
 Author: Kenneth C. Kleissl
 """
 
+
 class MatProp:
     """
-    A container for material properties by Kenneth C. Kleissl.
+    A container for material properties
 
     Attributes:
         f_ck: Characteristic strength of concrete cylinder
@@ -39,14 +37,37 @@ class MatProp:
         self.update_strengths()
         # self.tensile_strength()
 
-    def setMethods(self, conc_method, reinf_method):
-        self.conc_method = conc_method
-        self.reinf_method = reinf_method
-
     def update_strengths(self):
         self.f_cd = self.alpha_cc * self.f_ck / self.gamma_c
         self.f_yd = self.f_yk / self.gamma_s
         self.f_ct = 0.7 * 0.3 * self.f_ck ** (2 / 3)
+
+    def set_methods(self, conc_method, reinf_method):
+        self.conc_method = conc_method
+        self.reinf_method = reinf_method
+        self.update_conc_stiffness()  # some methods overrules the concrete stiffness
+
+    def update_conc_stiffness(self):
+        if self.conc_method == 'EN Bi-linear':
+            eps_c3 = 0.00175
+            self.E_cm = self.f_cd / eps_c3 / 1000
+        elif self.conc_method == 'EN Parabolic-rectangular':
+            eps = -0.00001
+            stress = self.concreteStress(eps)
+            self.E_cm = stress / eps / 1000  # calculate initial tanget stiffness
+        elif self.conc_method == 'Sudden plastic':
+            self.E_cm = 9999
+
+    def is_conc_stiffness_assignable(self):
+        """
+        This method is used by the GUI to grey out the concrete stiffness input cell if not applicable
+        """
+        if self.conc_method in ['EN Bi-linear', 'EN Parabolic-rectangular', 'Sudden plastic']:
+            assignable = False
+        else:
+            assignable = True
+
+        return assignable, self.E_cm
 
     # def setLimitState(self, limit_state):
     #     self.limit_state = limit_state
@@ -66,7 +87,6 @@ class MatProp:
     # Function for converting concrete strain to stress
     def concreteStress(self, strain):
         # input parameters
-        f_cd = self.f_cd
         E_cm = self.E_cm * 1000  # converting from GPa to MPa
         eps_c2 = 0.002
         n = 2
@@ -79,13 +99,13 @@ class MatProp:
         if eps_c < 0:  # is in tension
             sigma_c = 0
         elif eps_c <= eps_c2:
-            sigma_c = f_cd * (1 - (1 - eps_c / eps_c2) ** n)
+            sigma_c = self.f_cd * (1 - (1 - eps_c / eps_c2) ** n)
         elif eps_c <= eps_cu2:
-            sigma_c = f_cd
+            sigma_c = self.f_cd
         else:
             sigma_c = 0
 
-        ConcreteStress = -sigma_c # convert sign back before returning value
+        ConcreteStress = -sigma_c  # convert sign back before returning value
 
         # check if to overrule with EN Bi-linear behaviour
         if self.conc_method == "EN Bi-linear":
@@ -95,11 +115,12 @@ class MatProp:
             eps_cu3 = 0.0035
 
             # overwrite concrete stiffness
-            E_cm = f_cd / eps_c3
+            E_cm = self.f_cd / eps_c3                # <--- overwrites the user specified stiffness!
+
             ConcreteStress = E_cm * strain
-            if ConcreteStress < -f_cd:
-                if eps_c < eps_cu3:
-                    ConcreteStress = -f_cd
+            if ConcreteStress < -self.f_cd:
+                if eps_c <= eps_cu3:  # avoids the vertical part near eps_cu3
+                    ConcreteStress = -self.f_cd
                 else:
                     ConcreteStress = 0
             elif ConcreteStress > 0:
@@ -111,7 +132,7 @@ class MatProp:
             # load input parameters
             eps_c1 = 0.0022
             eps_cu1 = 0.0035
-            f_cm = f_cd + 8  # what about safety factor?
+            f_cm = self.f_cd + 8  # what about safety factor?
 
             eta = eps_c / eps_c1
             k = 1.05 * E_cm * abs(eps_c1) / f_cm
@@ -132,8 +153,8 @@ class MatProp:
         # check if to overrule with elastic-plastic behaviour
         if self.conc_method == "Elastic-plastic":
             ConcreteStress = E_cm * strain
-            if ConcreteStress < -f_cd:
-                ConcreteStress = -f_cd
+            if ConcreteStress < -self.f_cd:
+                ConcreteStress = -self.f_cd
             elif ConcreteStress > 0:
                 ConcreteStress = 0
 
@@ -141,7 +162,7 @@ class MatProp:
         if self.conc_method == "Sudden plastic":
             eps_c_pl = 0.002
             if eps_c >= eps_c_pl:
-                ConcreteStress = -f_cd
+                ConcreteStress = -self.f_cd
             else:
                 ConcreteStress = 0
 
@@ -151,16 +172,15 @@ class MatProp:
     def reinforcementStress(self, eps_s):
 
         # load input parameters
-        f_yd = self.f_yd
         E_s = self.E_s * 1000  # converting from GPa to MPa
-        eps_y = f_yd / E_s
+        eps_y = self.f_yd / E_s
         eps_su = 0.05
 
         # calculate reinforcement stress for elastic-plastic behaviour
-        if abs(eps_s) < eps_y: # is elastic
+        if abs(eps_s) < eps_y:  # is elastic
             sigma_s = E_s * eps_s
         elif abs(eps_s) <= eps_su:
-            sigma_s = eps_s / abs(eps_s) * f_yd
+            sigma_s = eps_s / abs(eps_s) * self.f_yd
         else:
             sigma_s = 0
 
@@ -171,14 +191,14 @@ class MatProp:
             if abs(eps_s) > eps_y and abs(eps_s) <= eps_su:
                 # load input parameters
                 k = 1.08
-                f_td = k*f_yd
+                f_td = k * self.f_yd
 
                 # adjust k-factor to reflect the given strain
                 k = 1 + (k - 1) * (abs(eps_s) - eps_y) / (eps_su - eps_y)
 
                 # calculate reinforcement stress
                 ReinforcementStress = k * ReinforcementStress
-                # sigma_s = k * f_yd
+                # sigma_s = k * self.f_yd
                 # ReinforcementStress = eps_s / Abs(eps_s) * sigma_s
 
         # check if to overrule with linear elastic behaviour
@@ -197,5 +217,4 @@ class MatProp:
 if __name__ == '__main__':
 
     Mat = MatProp()
-    print(Mat.f_ck, Mat.f_ct, Mat.f_yk, Mat.concreteStress(-0.002), Mat.reinforcementStress(-0.002))
-
+    print(Mat.f_ck, Mat.f_ct, Mat.f_yk, Mat.concreteStress(-0.002), Mat.reinforcementStress(-0.002), Mat.E_cm)

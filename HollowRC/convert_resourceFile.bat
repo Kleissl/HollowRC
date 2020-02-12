58a26b41268b36541c84af4e852735140fd79959
Kenneth C. Kleissl <kenneth_kleissl@hotmail.com>
2020-02-13 00:40:43 +0100

    more generic paths for resource converter script

--- HollowRC/convert_resourceFile.bat
+++ HollowRC/convert_resourceFile.bat
@@ -1,6 +1,6 @@
-"C:\Users\Kenneth\Anaconda3\Library\bin\pyside2-rcc.exe" -o resources_rc.py resources.qrc
-"C:\Users\kekl\Anaconda3\Library\bin\pyside2-rcc.exe" -o resources_rc.py resources.qrc
-"C:\Users\kekl\AppData\Local\Programs\Python\Python37-32\Lib\site-packages\PySide2\pyside2-rcc.exe" -o resources_rc.py resources.qrc
-"C:\Users\kekl\AppData\Local\Programs\Python\Python37\Lib\site-packages\PySide2\pyside2-rcc.exe" -o resources_rc.py resources.qrc
+"C:\Program Files (x86)\Anaconda3\Library\bin\pyside2-rcc.exe" -o resources_rc.py resources.qrc
+"%LOCALAPPDATA%\Programs\Anaconda3\Library\bin\pyside2-rcc.exe" -o resources_rc.py resources.qrc
 "C:\Program Files (x86)\Python37-32\Lib\site-packages\PySide2\pyside2-rcc.exe" -o resources_rc.py resources.qrc
+"%LOCALAPPDATA%\Programs\Python\Python37-32\Lib\site-packages\PySide2\pyside2-rcc.exe" -o resources_rc.py resources.qrc
+"%LOCALAPPDATA%\Programs\Python\Python37\Lib\site-packages\PySide2\pyside2-rcc.exe" -o resources_rc.py resources.qrc
 pause

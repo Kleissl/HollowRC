f878910f0b82e1cc5e1d938f54f9ab689082118c
Kenneth C. Kleissl <kenneth_kleissl@hotmail.com>
2021-02-11 23:24:17 +0100

    upgrade pyside2, script + recompiled resources

--- HollowRC/convert_resourceFile.bat
+++ HollowRC/convert_resourceFile.bat
@@ -1,6 +1,3 @@
-"C:\Program Files (x86)\Anaconda3\Library\bin\pyside2-rcc.exe" -o resources_rc.py resources.qrc
-"%LOCALAPPDATA%\Programs\Anaconda3\Library\bin\pyside2-rcc.exe" -o resources_rc.py resources.qrc
-"C:\Program Files (x86)\Python37-32\Lib\site-packages\PySide2\pyside2-rcc.exe" -o resources_rc.py resources.qrc
-"%LOCALAPPDATA%\Programs\Python\Python37-32\Lib\site-packages\PySide2\pyside2-rcc.exe" -o resources_rc.py resources.qrc
-"%LOCALAPPDATA%\Programs\Python\Python37\Lib\site-packages\PySide2\pyside2-rcc.exe" -o resources_rc.py resources.qrc
+"%LOCALAPPDATA%\Programs\Python\Python37-32\Scripts\pyside2-rcc.exe" -o resources_rc.py resources.qrc
+"%LOCALAPPDATA%\Programs\Python\Python37\Scripts\pyside2-rcc.exe" -o resources_rc.py resources.qrc
 pause

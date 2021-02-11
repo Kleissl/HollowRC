725e2710dc5648bcc19750fdec354e5f17706777
Kenneth C. Kleissl <kenneth_kleissl@hotmail.com>
2021-02-11 23:21:55 +0100

    upgrade pyside2, UI compiler option --from-imports

--- HollowRC/convert_ui.bat
+++ HollowRC/convert_ui.bat
@@ -1,4 +1,2 @@
-"C:\Program Files (x86)\Python37-32\Scripts\pyside2-uic.exe" -x hollow_window.ui -o hollow_window.py
-"%LOCALAPPDATA%\Programs\Python\Python37-32\Scripts\pyside2-uic.exe" -x hollow_window.ui -o hollow_window.py
-"%LOCALAPPDATA%\Programs\Python\Python37\Scripts\pyside2-uic.exe" -x hollow_window.ui -o hollow_window.py
+"%LOCALAPPDATA%\Programs\Python\Python37\Scripts\pyside2-uic.exe" --from-imports hollow_window.ui -o hollow_window.py
 pause

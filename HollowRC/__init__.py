d656a18fecf82bedf996b774aaaa8bba4a8eed73
Kenneth C. Kleissl <kenneth_kleissl@hotmail.com>
2021-02-11 22:26:55 +0100

    extended __init__ with user-facing classes

--- HollowRC/__init__.py
+++ HollowRC/__init__.py
@@ -3,3 +3,7 @@ Package init
 Created by Kenneth C. Kleissl:
     https://github.com/Kleissl
 """
+from .Geometry import CrossSection, Wall
+from .Material import MatProp
+from .SectionForces import SectionForces
+# from .Analysis import Analysis
